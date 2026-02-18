3988982424d8fae6d5f573512fd0b8d6ef86a572
Arseniy-pixar <nekrutenkoarseniy@gmail.com>
2026-02-18 12:15:50 +0300

    Update queries .sql

--- queries .sql
+++ queries .sql
@@ -1,4 +1,4 @@
-ЗАПРОС 1
+ЗАПРОС 4
 select concat(c.first_name,' ', c.last_name) as seller, -- создаёт столбец 'seller' с именем и фамилией продавца, объединёнными через пробел
 count(p.price * s.quantity) as operation, -- считает количество операций (продаж), в данном случае — количество строк, умноженных на цену, что некорректно — лучше считать строки или сумму продаж
 sum(p.price * s.quantity) as income -- сумму выручки продавца
@@ -10,7 +10,7 @@ group by c.first_name, c.last_name -- группирует по имени и ф
 order by income desc -- сортирует по выручке в порядке убывания
 limit 10 -- выводит только первые 10 записей
   
-ЗАПРОС 2
+ЗАПРОС 5
 with overall_average as ( -- объявляем временную таблицу (CTE) с именем overall_average
 select round(avg(p.price * s.quantity), 0) as avg_income -- считаем среднюю выручку по всем продажам, округляем до целого, присваиваем alias avg_income
 from customers c -- из таблицы клиентов
@@ -28,7 +28,7 @@ group by e.first_name, e.last_name, o.avg_income -- группируем по п
 having round(avg(p.price * s.quantity), 0) < o.avg_income -- фильтрация: выбираем только тех продавцов, у которых средняя выручка за сделку меньше общей
 order by average_income asc -- сортируем по средней выручке по возрастанию
   
-ЗАПРОС 3
+ЗАПРОС 6
 select concat(e.first_name,' ', e.last_name) as seller, -- формирует столбец 'seller' с именем и фамилией продавца
 trim(to_char(s.sale_date, 'Day')) as day_of_week, -- название дня недели (на английском), с удалением лишних пробелов
 sum(p.price * s.quantity) as income -- вычисляет общую сумму выручки за все сделки продавца за выбранный день
