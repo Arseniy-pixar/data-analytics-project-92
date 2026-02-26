d975c0b64a15e81ca4109c4be3925949effb4c88
Arseniy-pixar <nekrutenkoarseniy@gmail.com>
2026-02-26 11:54:58 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,33 +11,45 @@ order by 1 -- сортирует результат по тому же полю,
 
  -- top_10_total_income
 
-select concat(c.first_name,' ', c.last_name) as seller, -- создаёт столбец 'seller' с именем и фамилией продавца, объединёнными через пробел
-count(p.price * s.quantity) as operation, -- считает количество операций (продаж), в данном случае — количество строк, умноженных на цену, что некорректно — лучше считать строки или сумму продаж
-floor(sum(p.price * s.quantity)) as income -- сумма выручки продавца, округлённая в меньшую сторону
+select
+    concat(c.first_name, ' ', c.last_name) as seller, -- создаёт столбец 'seller' с именем и фамилией продавца, объединёнными через пробел
+    count(p.price * s.quantity) as operation, -- считает количество операций (продаж), в данном случае — количество строк, умноженных на цену, что некорректно — лучше считать строки или сумму продаж
+    -- сумма выручки продавца, округлённая в меньшую сторону
+    floor(sum(p.price * s.quantity)) as income
 from customers as c -- таблица клиентов с алиасом c
-inner join sales as s on c.customer_id = s.customer_id -- соединяет таблицы клиентов и продаж по идентификатору клиента
-join products as p on s.product_id = p.product_id -- соединяет таблицы продаж и продуктов по идентификатору товара
-join employees as e on e.employee_id = s.sales_person_id
+-- соединяет таблицы клиентов и продаж по идентификатору клиента
+inner join sales as s on c.customer_id = s.customer_id
+-- соединяет таблицы продаж и продуктов по идентификатору товара
+inner join products as p on s.product_id = p.product_id
+inner join employees as e on s.sales_person_id = e.employee_id
 group by c.first_name, c.last_name -- группирует по имени и фамилии продавца
 order by income desc -- сортирует по выручке в порядке убывания
 limit 10 -- выводит только первые 10 записей
 
  -- lowest_average_income
 
-with overall_average as ( -- объявляем временную таблицу (CTE) с именем overall_average
-select avg(p.price * s.quantity) as avg_income -- считаем среднюю выручку по всем продажам, округляем до целого, присваиваем alias avg_income
-from customers c -- из таблицы клиентов
-join sales s on c.customer_id = s.customer_id -- присоединяем продажи по идентификатору клиента
-join products p on s.product_id = p.product_id -- присоединяем товары по идентификатору товара
+-- объявляем временную таблицу (CTE) с именем overall_average
+with overall_average as (
+    select avg(p.price * s.quantity) as avg_income -- считаем среднюю выручку по всем продажам, округляем до целого, присваиваем alias avg_income
+    from customers as c -- из таблицы клиентов
+    -- присоединяем продажи по идентификатору клиента
+    inner join sales as s on c.customer_id = s.customer_id
+    -- присоединяем товары по идентификатору товара
+    inner join products as p on s.product_id = p.product_id
 )
-select concat(e.first_name, ' ', e.last_name) as seller, -- формируем название продавца (имя + фамилия)
-       floor(avg(p.price * s.quantity)) as average_income -- считаем среднюю выручку продавца за сделки, округляем
-from customers c -- таблица клиентов
-join sales s on c.customer_id = s.customer_id -- присоединение продаж
-join products p on s.product_id = p.product_id -- присоединение товаров
-join employees as e on e.employee_id = s.sales_person_id
-cross join overall_average o -- кросс-присоединение с общей средней, чтобы получить это значение в каждой строке
-group by e.first_name, e.last_name, o.avg_income -- группируем по продавцам и по общей средней выручке
+
+select
+    -- формируем название продавца (имя + фамилия)
+    concat(e.first_name, ' ', e.last_name) as seller,
+    -- считаем среднюю выручку продавца за сделки, округляем
+    floor(avg(p.price * s.quantity)) as average_income
+from customers as c -- таблица клиентов
+inner join sales as s on c.customer_id = s.customer_id -- присоединение продаж
+inner join products as p on s.product_id = p.product_id -- присоединение товаров
+inner join employees as e on s.sales_person_id = e.employee_id
+cross join overall_average as o -- кросс-присоединение с общей средней, чтобы получить это значение в каждой строке
+-- группируем по продавцам и по общей средней выручке
+group by e.first_name, e.last_name, o.avg_income
 having avg(p.price * s.quantity) < o.avg_income -- фильтрация: выбираем только тех продавцов, у которых средняя выручка за сделку меньше общей
 order by average_income asc -- сортируем по средней выручке по возрастанию
 
